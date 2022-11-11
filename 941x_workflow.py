001c62ffe2223987ae18e18dd3dafa25194bc536
Daniel Guimaraes <dguimaraesllc@gmail.com>
2022-11-11 11:48:42 -0600

    Fixed bugs in WS creation

--- 941x_workflow.py
+++ 941x_workflow.py
@@ -49,30 +49,31 @@ def get_company_paths(company: str):
     return res
 
 
-def copy_worksheet(dest_dir: str, pdf_path: str):
+def copy_worksheet(dest_dir: str, pdf_path: str, company: str):
     for item in os.listdir(pdf_path):
         if conf["base_ws_name"] in item:
             shutil.copy(
                 os.path.join(pdf_path, item),
                 dest_dir,
             )
-            new_name = f"{conf['company']} ERTC Worksheet.xlsx"
+            new_name = f"{company} ERTC Worksheet.xlsx"
             os.rename(os.path.join(dest_dir, item), os.path.join(dest_dir, new_name))
+            break
 
 
-def set_up_worksheet(company_path: str, pdf_path: str):
+def set_up_worksheet(company_path: str, pdf_path: str, company: str):
     new_dir_name = os.path.join(company_path, "Payroll And Worksheet")
     os.rename(
         os.path.join(company_path, "Payroll"),
         new_dir_name,
     )
-    copy_worksheet(new_dir_name, pdf_path)
+    copy_worksheet(new_dir_name, pdf_path, company)
 
 
-def build_wb_path(company_path: str, pdf_path: str):
+def build_wb_path(company_path: str, pdf_path: str, company: str):
     if os.path.exists(os.path.join(company_path, "Payroll")):
         # Then set up the file and exit
-        set_up_worksheet(company_path, pdf_path)
+        set_up_worksheet(company_path, pdf_path, company)
         sys.exit(0)
     for item in os.listdir(company_path):
         if item.lower() in conf["payroll_dirs"]:
@@ -102,7 +103,7 @@ if __name__ == "__main__":
             f941x_path = os.path.join(pdf_path, conf["f941x_file_name"])
             f8821_path = os.path.join(pdf_path, conf["f8821_file_name"])
             output_path = os.path.join(company_path, "941x")
-            wb_path = build_wb_path(company_path, pdf_path)
+            wb_path = build_wb_path(company_path, pdf_path, company)
             excel = excel_helper(wb_path, SHEETS, ROUND_DELTA, ROW_2020, ROW_2021)
             excel.load_data()
             pdf = pdf_helper(
